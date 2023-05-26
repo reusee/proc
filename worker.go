756e108f4f50533358184c63e71189efd6b70573
reus <reusee@gmail.com>
2023-05-26 13:02:26 +0800

    add Worker

---
+++ worker.go
@@ -0,0 +1,104 @@
+package proc
+
+import (
+	"context"
+	"sync"
+)
+
+type Worker struct {
+	mutex       sync.Mutex
+	cond        *sync.Cond
+	queue       *ProcQueue
+	ctx         context.Context
+	cancel      context.CancelFunc
+	newProcChan chan Proc
+	next        Next
+	err         error
+}
+
+func NewWorker(ctx context.Context) *Worker {
+	ctx, cancel := context.WithCancel(ctx)
+	ret := &Worker{
+		queue:       NewProcQueue(),
+		ctx:         ctx,
+		cancel:      cancel,
+		newProcChan: make(chan Proc),
+	}
+	ret.cond = sync.NewCond(&ret.mutex)
+	go ret.start(ctx)
+	return ret
+}
+
+func (w *Worker) start(ctx context.Context) {
+	defer func() {
+		w.mutex.Lock()
+		defer w.mutex.Unlock()
+		w.queue = nil
+		w.cond.Broadcast()
+	}()
+
+loop:
+	for {
+
+		if w.queue.empty() {
+			select {
+			case <-ctx.Done():
+				break loop
+			case proc := <-w.newProcChan:
+				w.queue.enqueue(proc)
+			}
+
+		} else {
+			select {
+			case <-ctx.Done():
+				break loop
+			case proc := <-w.newProcChan:
+				w.queue.enqueue(proc)
+			default:
+				w.next.reset()
+				proc, _ := w.queue.dequeue()
+				err := proc.Run(&w.next)
+				if err != nil {
+					w.mutex.Lock()
+					w.err = err
+					w.mutex.Unlock()
+					w.cancel()
+					return
+				}
+				for _, newProc := range w.next.procs {
+					w.queue.enqueue(newProc)
+				}
+			}
+		}
+
+	}
+
+	// do rest works
+	if err := w.queue.RunAll(); err != nil {
+		w.mutex.Lock()
+		w.err = err
+		w.mutex.Unlock()
+		w.cancel()
+		return
+	}
+
+}
+
+func (w *Worker) Do(proc Proc) error {
+	select {
+	case <-w.ctx.Done():
+		return w.ctx.Err()
+	case w.newProcChan <- proc:
+	}
+	return nil
+}
+
+func (w *Worker) Close() error {
+	w.cancel()
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	for w.queue != nil {
+		w.cond.Wait()
+	}
+	return w.err
+}
---
+++ worker_test.go
@@ -0,0 +1,110 @@
+package proc
+
+import (
+	"context"
+	"errors"
+	"io"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestWorker(t *testing.T) {
+	n := 0
+	max := 1024
+	var proc ProcFunc
+	proc = func(next *Next) error {
+		n++
+		if n == max {
+			return nil
+		}
+		next.Add(proc)
+		return nil
+	}
+
+	w := NewWorker(context.Background())
+	if err := w.Do(proc); err != nil {
+		t.Fatal(err)
+	}
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+	if n != max {
+		t.Fatalf("got %d", n)
+	}
+
+	err := w.Do(nil)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal()
+	}
+}
+
+func TestWorkerError(t *testing.T) {
+	w := NewWorker(context.Background())
+	if err := w.Do(ProcFunc(func(_ *Next) error {
+		return io.EOF
+	})); err != nil {
+		t.Fatal(err)
+	}
+	err := w.Close()
+	if !errors.Is(err, io.EOF) {
+		t.Fatal()
+	}
+	err = w.Close()
+	if !errors.Is(err, io.EOF) {
+		t.Fatal()
+	}
+}
+
+func TestWorkerConcurrent(t *testing.T) {
+	w := NewWorker(context.Background())
+	wg := new(sync.WaitGroup)
+	n := 1024
+	var c atomic.Int64
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			if err := w.Do(ProcFunc(func(_ *Next) error {
+				c.Add(1)
+				return nil
+			})); err != nil {
+				panic(err)
+			}
+		}()
+	}
+	wg.Wait()
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+	if c.Load() != 1024 {
+		t.Fatal()
+	}
+}
+
+func TestWorkerCanceledCtx(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	w := NewWorker(ctx)
+	cancel()
+	err := w.Do(nil)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal()
+	}
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel = context.WithCancel(context.Background())
+	cancel()
+	w = NewWorker(ctx)
+	err = w.Do(nil)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal()
+	}
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
