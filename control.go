5ad69b4476bb973f113edb253d6565353ff13669
reus <reusee@gmail.com>
2023-11-02 23:14:22 +0800

    code clean-ups

---
+++ control.go
@@ -0,0 +1,24 @@
+package proc
+
+import "github.com/reusee/pr2"
+
+type Control struct {
+	procs []Proc
+}
+
+func (n *Control) Next(proc Proc) {
+	n.procs = append(n.procs, proc)
+}
+
+func (n *Control) reset() {
+	n.procs = n.procs[:0]
+}
+
+var controlsPool = pr2.NewPool(
+	128,
+	func() *Control {
+		return &Control{
+			procs: make([]Proc, 0, 8),
+		}
+	},
+)
--- proc.go
+++ proc.go
@@ -1,28 +1,5 @@
 package proc
 
-import "github.com/reusee/pr2"
-
 type Proc interface {
 	Step(*Control)
 }
-
-type Control struct {
-	procs []Proc
-}
-
-func (n *Control) Next(proc Proc) {
-	n.procs = append(n.procs, proc)
-}
-
-func (n *Control) reset() {
-	n.procs = n.procs[:0]
-}
-
-var controlsPool = pr2.NewPool(
-	128,
-	func() *Control {
-		return &Control{
-			procs: make([]Proc, 0, 8),
-		}
-	},
-)
